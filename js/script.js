02cf337b8fc2554118f117ce9cf6a24157e98bce
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-22 16:48:58 +0200

    Esercizio milestone svolto.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,22 @@ const teamMembers = {
     position: ["Founder & CEO", "Chief Editor", "Office Manager", "Social Media Manager", "Developer", "Graphic Designer"],
     photo: ["wayne-barnett-founder-ceo.jpg", "angela-caroll-chief-editor.jpg", "walter-gordon-office-manager.jpg", "angela-lopez-social-media-manager.jpg", "scott-estrada-developer.jpg", "barbara-ramos-graphic-designer.jpg"]
 }
-
+const divAppend = document.querySelector("div.container");
+let cardEl;
+let cardDescriptionEl;
 for(let i = 0; i < teamMembers.nome.length; i++){
     console.log(`Nome:${teamMembers.nome[i]}`,`Ruolo:${teamMembers.position[i]}`,`Foto:${teamMembers.photo[i]}`);
+    // createEl(cardEl, divAppend, "article",`Nome:${teamMembers.nome[i]} `+` Ruolo:${teamMembers.position[i]} `+` Foto:${teamMembers.photo[i]} `)
+    let articleAppend = document.createElement("article");
+    // let nomeHTwoEl = createEl(cardDescriptionEl, articleAppend, "h2", "ciao")
+    articleAppend = createEl(cardEl, divAppend, "article", createEl(cardDescriptionEl, articleAppend, "h2", "ciao"));
 }
-    
+
+
+
+//*****************FUNZIONI****************//
+function createEl(elemento, elementoAppend, tag, qualcosa){
+    elemento = document.createElement(tag);
+    elemento.append(qualcosa);
+    elementoAppend.appendChild(elemento);
+}
